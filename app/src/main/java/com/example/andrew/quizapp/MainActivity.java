548fc1e32c51a3f8d78327a4d9e1315d228b31ec
anderw emad <anderwemad@hotmail.com>
2017-09-23 03:02:06 +0200

    Quiz App Complete

--- app/src/main/java/com/example/andrew/quizapp/MainActivity.java
+++ app/src/main/java/com/example/andrew/quizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.andrew.quizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -15,55 +16,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void selectitem(View view) {
-
-
-        CheckBox right = (CheckBox) findViewById(R.id.answer_question1a);
-        boolean check = right.isChecked();
-        CheckBox right2 = (CheckBox) findViewById(R.id.answer_question1b);
-        boolean checkk = right2.isChecked();
-        if (check && checkk) {
-            score++;
+        CheckBox checkA = (CheckBox) findViewById(R.id.answer_question1a);
+        CheckBox checkB = (CheckBox) findViewById(R.id.answer_question1b);
+        if (checkA.isChecked() && checkB.isChecked()) {
+            score += 1;
+        } else {
+            return;
         }
-
-
     }
 
-
-
-
     public void onRadioButtonClicked(View view) {
-
-
         RadioButton right = (RadioButton) findViewById(R.id.answer_question4b);
         boolean check = right.isChecked();
         if (check) {
-            score++;
+            score += 1;
+        }
+        else {
+            return;
+        }
+    }
+
+    public void questionThree (View view){
+        EditText text = (EditText) findViewById(R.id.answer_question2);
+        String value = text.getText().toString();
+        String result = "Past Tense";
+        if (value.equals(result)) {
+            score += 1;
+        } else {
+            return;
         }
 
+    }
+
+    public void questionFour(View view){
+
+        EditText textt = (EditText) findViewById(R.id.answer_question3);
+        String value1 = textt.getText().toString();
+        String result = "Future Tense";
+        if (value1.equals(result)) {
+            score += 1;
+        } else {
+            return;
+        }
     }
 
     public void Submit(View view) {
-        EditText text = (EditText) findViewById(R.id.answer_question2);
-        String value = text.getText().toString();
-        String result ="Past Tense";
-        if (value.equals(result) ) {
-            score++;
+        questionThree(view);
+        questionFour(view);
+        if (score <= 4) {
+            display(score);
+        } else {
+            return;
         }
-        EditText textt = (EditText) findViewById(R.id.answer_question3);
-        String value1 = textt.getText().toString();
-        String resultt ="Future Tense";
-        if (value1.equals(resultt)) {
-            score++;
-        }
-        display(score);
     }
-
-    public void display(int scores) {
-        TextView scoree = (TextView) findViewById(R.id.Result);
-        scoree.setText(String.valueOf(score));
+    public void display(int scoree) {
+        TextView scores = (TextView) findViewById(R.id.Result);
+        scores.setText(String.valueOf(scoree));
 
     }
 }
