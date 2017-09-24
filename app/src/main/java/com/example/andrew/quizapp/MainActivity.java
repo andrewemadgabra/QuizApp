0ccd10059aa162c1bb7bf8e143742c62d9bacb17
anderw emad <anderwemad@hotmail.com>
2017-09-25 00:48:01 +0200

    Quiz App Complete

--- app/src/main/java/com/example/andrew/quizapp/MainActivity.java
+++ app/src/main/java/com/example/andrew/quizapp/MainActivity.java
@@ -10,25 +10,22 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int score = 0;
-    Button button1;
-    EditText answer1 ,answer2;
+    int score1,score2,score3,score4,final_grade ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        answer1=(EditText) findViewById(R.id.et_answer_question2);
-        answer2=(EditText) findViewById(R.id.et_answer_question3);
-        button1=(Button) findViewById(R.id.submit_button);
     }
 
     public void selectitem(View view) {
         CheckBox checkA = (CheckBox) findViewById(R.id.cb_answer_question1a);
         CheckBox checkB = (CheckBox) findViewById(R.id.cb_answer_question1b);
-        if (checkA.isChecked() && checkB.isChecked()) {
-            score += 1;
+        CheckBox checkC = (CheckBox) findViewById(R.id.cb_answer_question1c);
+        CheckBox checkD = (CheckBox) findViewById(R.id.cb_answer_question1d);
+        if (checkA.isChecked() && checkB.isChecked() && !checkC.isChecked() && !checkD.isChecked()) {
+            score1 += 1;
         } else {
-            return;
+            score1 = 0;
         }
     }
 
@@ -37,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
         String value = text.getText().toString();
         String result = "Past Tense";
         if (value.equals(result)) {
-            score += 1;
+            score2 += 1;
         } else {
-            return;
+            score2 = 0;
         }
     }
 
@@ -48,37 +45,35 @@ public class MainActivity extends AppCompatActivity {
         String value1 = textt.getText().toString();
         String result = "Future Tense";
         if (value1.equals(result)) {
-            score += 1;
+            score3 += 1;
         } else {
-            return;
+            score3 = 0;
         }
     }
 
     public void onRadioButtonClicked(View view) {
         RadioButton right = (RadioButton) findViewById(R.id.rb_answer_question4b);
         if (right.isChecked()) {
-            score += 1;
+            score4 += 1;
         } else {
-            return;
+            score4 = 0;
         }
     }
 
     public void Submit(View view) {
+        CharSequence resultsDisplay;
         questionTWO(view);
         questionTHREE(view);
-        if (score <= 4) {
-            display(score);
+        final_grade = score1+score2+score3+score4;
+        if (final_grade == 10) {
+            resultsDisplay = "Perfect! You scored 4 out of 4";
         } else {
-            return;
+            resultsDisplay = "Try again. You scored " + final_grade + " out of 4";
         }
-    }
-
-    public void display(int scoree) {
-        Toast.makeText(getApplicationContext(), "Your Result : " + score, Toast.LENGTH_LONG).show();
-        EditText clear=(EditText) findViewById(R.id.et_answer_question3);
+        Toast.makeText(getApplicationContext(), "Your Result : " + resultsDisplay, Toast.LENGTH_LONG).show();
+        EditText clear = (EditText) findViewById(R.id.et_answer_question3);
         clear.setText("");
-        EditText delete=(EditText) findViewById(R.id.et_answer_question2);
+        EditText delete = (EditText) findViewById(R.id.et_answer_question2);
         delete.setText("");
-        score=0;
     }
 }
