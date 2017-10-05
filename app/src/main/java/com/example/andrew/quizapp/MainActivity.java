9c009360a8ade6af230d089e035ec565d54e76d0
anderw emad <anderwemad@hotmail.com>
2017-10-05 11:32:59 +0200

    will finish

--- app/src/main/java/com/example/andrew/quizapp/MainActivity.java
+++ app/src/main/java/com/example/andrew/quizapp/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.andrew.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.Toast;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int final_grade = 0;
     RadioGroup mRadioGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void selectitem(View view) {
         CheckBox checkA = (CheckBox) findViewById(R.id.cb_answer_question1a);
         CheckBox checkB = (CheckBox) findViewById(R.id.cb_answer_question1b);
@@ -25,52 +26,55 @@ public class MainActivity extends AppCompatActivity {
         CheckBox checkD = (CheckBox) findViewById(R.id.cb_answer_question1d);
         if (checkA.isChecked() && checkB.isChecked() && !checkC.isChecked() && !checkD.isChecked()) {
             final_grade += 1;
-        } else {
-            return;
         }
     }
 
     public void questionTWO(View view) {
         EditText text = (EditText) findViewById(R.id.et_answer_question2);
         String value = text.getText().toString();
-        String result = "Past Tense";
-        if (value.equals(result)) {
+        String resultone = "Past Tense";
+	String resulttwo = "past tense";
+        if (value.equals(resultone)||value.equals(resulttwo)) {
             final_grade += 1;
-        } else {
-            return;
         }
     }
 
     public void questionTHREE(View view) {
         EditText textt = (EditText) findViewById(R.id.et_answer_question3);
         String value1 = textt.getText().toString();
-        String result = "Future Tense";
-        if (value1.equals(result)) {
+        String resultone = "Future Tense";
+	String resulttwo = "future tense";
+        if (value1.equals(resultone)||value1.equals(resulttwo)) {
             final_grade += 1;
-        } else {
-            return;
         }
     }
+
     public void onRadioButtonClicked(View view) {
-        RadioButton right = (RadioButton) findViewById(R.id.rb_answer_question4b);
-        if (right.isChecked()) {
+        RadioButton answer1 = (RadioButton) findViewById(R.id.rb_answer_question4a);
+        RadioButton answer2 = (RadioButton) findViewById(R.id.rb_answer_question4b);
+        RadioButton answer3 = (RadioButton) findViewById(R.id.rb_answer_question4c);
+        Boolean answer;
+        answer=answer2.isChecked();
+        if (answer) {
             final_grade += 1;
-        } else {
-            return;
         }
+
     }
+
     public void Submit(View view) {
         String Display;
         questionTWO(view);
         questionTHREE(view);
         if (final_grade < 4) {
             Display = "Try again. You scored " + final_grade + " out of 4";
-            Toast.makeText(getApplicationContext(), "Your Result : " + Display, Toast.LENGTH_LONG).show();
-            final_grade=0;
-        } else if(final_grade==4) {
+            Toast.makeText(getApplicationContext(), "Your Result : " + Display, Toast.LENGTH_SHORT).show();
+            final_grade = 0;
+        } else if (final_grade == 4) {
             Display = "You scored " + final_grade + " out of 4";
-            Toast.makeText(getApplicationContext(), "Your Result : " + Display, Toast.LENGTH_LONG).show();
-            final_grade=0;
+            Toast.makeText(getApplicationContext(), "Your Result : " + Display, Toast.LENGTH_SHORT).show();
+            final_grade = 0;
+        } else {
+            final_grade = 0;
         }
         CheckBox checkA = (CheckBox) findViewById(R.id.cb_answer_question1a);
         CheckBox checkB = (CheckBox) findViewById(R.id.cb_answer_question1b);
